2ba511b964b4e96a0567f92c555bf31438b071c2
Marcelo Canaparro <mcanaparro@weg.net>
2021-03-06 18:14:36 -0300

    clean up

--- src/main/java/com/canaparro/App.java
+++ src/main/java/com/canaparro/App.java
@@ -1,8 +1,5 @@
 package com.canaparro;
 
-/**
- * Hello world!
- */
 public class App {
 
     public static void main(String[] args) {
--- src/test/java/com/canaparro/AppTest.java
+++ src/test/java/com/canaparro/AppTest.java
@@ -4,15 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue() {
+    public void test() {
         assertTrue(true);
     }
 }
